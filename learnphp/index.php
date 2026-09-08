9b314af61e94ff012b5ee1b05117d8f3daadf6a9
Danel Filippov <Danel.Filippov@tptlive.ee>
2026-09-08 09:25:03 +0300

    add array

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,14 +1,18 @@
 <?php
-$test = 10 > 5;
-$test = 10 < 5;
-$test = 10 == 10;
-$test = 10 <= 10;
-$test = 10 >= 10;
-$test = 10 == "10";
-$test = 10 === "10";
-$test = 10 != 10;
-$test = 10 !== 10;
-$test = 0 == false;
-
-var_dump($test)
+$test = array(1, 2, 3);
+$test = [1, 2, 3];
+//var_dump($test[3][1]);
+$test = [1, '123a', [1, 2]];
+$test = [
+    1,
+    'name' => 'kaspar',
+    'age' => 33,
+    100 => 'lol',
+    'troll'
+];
+array_push($test, 'added', 123);
+$test[] = 'new';
+$test['name'] = 'martin';
+unset($test[3]);
+var_dump($test);
 ?>
