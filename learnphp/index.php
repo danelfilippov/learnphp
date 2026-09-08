08609bf2117c9eecad41aa504279e9c2f3bcd5bc
Danel Filippov <Danel.Filippov@tptlive.ee>
2026-09-08 09:40:44 +0300

    Add if and switch exmaples

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,18 +1,47 @@
 <?php
-$test = array(1, 2, 3);
-$test = [1, 2, 3];
-//var_dump($test[3][1]);
-$test = [1, '123a', [1, 2]];
-$test = [
-    1,
-    'name' => 'kaspar',
-    'age' => 33,
-    100 => 'lol',
-    'troll'
-];
-array_push($test, 'added', 123);
-$test[] = 'new';
-$test['name'] = 'martin';
-unset($test[3]);
-var_dump($test);
+$num = 10;
+if($num > 10){
+    var_dump('Bigger');
+};
+
+if($num > 10){
+    var_dump('Bigger');
+}else{
+    var_dump('Smaller');
+};
+
+if($num > 10){
+    var_dump('Bigger');
+}elseif($num < 10){
+    var_dump('snammer');
+}else{
+    var_dump('Equal');
+};
+$day = (int)date('w');
+if ($day === 0){
+    var_dump('sunday');
+}elseif($day === 1){
+    var_dump('monday');
+}elseif($day === 2){
+    var_dump('tuesday');
+}elseif($day === 3){
+    var_dump('wednesday');
+}elseif($day === 4){
+    var_dump('thursday');
+}elseif($day === 5){
+    var_dump('friday');
+}elseif($day === 6){
+    var_dump('saturday');
+};
+
+switch($day){
+    case 0:
+        var_dump('sunday');
+        break;
+    case 1:
+        var_dump('monday');
+        break;
+    default:
+        var_dump('noday');
+};
 ?>
