4be7b07faf9e827c6e6fd6c1159cbce36f9ae0a9
Danel Filippov <Danel.Filippov@tptlive.ee>
2026-09-08 08:48:09 +0300

    Add math example

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,4 +1,16 @@
 <?php
-#$test = 10
-$test = 10
+$test = 10;
+$test = 10 + 10;
+$test = 10 - 9;
+$test = 10 / 1;
+$test = 10 * 1;
+$test = $test / 1;
+$test += 3;
+$test *= 3;
+
+$a = 1;
+$b = $a++;
+var_dump($a);
+$test = sin(pi());
+var_dump($test);
 ?>
