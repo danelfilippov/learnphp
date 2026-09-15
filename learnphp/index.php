4f9222bf1d995c3c52ac939f36c26bf97896ac1b
danelfilippov <danel.filippov@techno.ee>
2026-09-15 14:48:59 +0300

    add inheretencses

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,55 +1,38 @@
 <?php
+
 class Box
 {
+
     public $isOpen = false;
     public $hasBeenOpened = false;
 
-    public function __construct(public $width, public $height, public $length)
+    public function __construct(public $width, public $hight, public $length)
     {
-
-        var_dump('obj created');
+        var_dump('Object Created');
     }
+
 
     public function open()
     {
         $this->isOpen = true;
     }
-    public function __call($name, $arguments)
-    {
-        var_dump($name, $arguments);
-    }
-    public function __toString()
-    {
-        return 'im a box';
-    }
-    public function __get($name)
-    {
-        var_dump($name);
-        return 'cool value';
-    }
-    public function __set($name, $value)
-    {
-        var_dump($name, $value);
-    }
-    public function __destruct()
-    {
-        var_dump('obj destroyed');
-    }
-    public function __invoke()
-    {
-        var_dump('im a function');
-    }
-}
-function test()
-{
-    $box2 = new Box(2, 4, 6);
-}
-test();
 
-$box1 = new Box(1, 2, 3);
-var_dump($box1);
-echo $box1;
-$box1->mew = 'lol';
-var_dump($box1->coolio);
-$box1();
-$box1->hello(1, 'asdas', 3);
+    public function volume()
+    {
+        return $this->width * $this->hight * $this->length;
+    }
+}
+
+class Metalbox extends Box
+{
+    public $weightPerUnit;
+
+    public function mass()
+    {
+        return $this->weightPerUnit * $this->volume();
+    }
+}
+
+$metal1 = new Metalbox(1, 2, 3);
+$metal1->weightPerUnit = 1;
+var_dump($metal1->mass(), $metal1);
