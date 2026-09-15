23102e5815f4933f66619c335b64e151cca35f86
danelfilippov <danel.filippov@techno.ee>
2026-09-15 09:37:11 +0300

    Add magic method examples

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,32 +1,55 @@
 <?php
 class Box
 {
-    public $width;
-    public $height;
-    public $length;
     public $isOpen = false;
     public $hasBeenOpened = false;
+
+    public function __construct(public $width, public $height, public $length)
+    {
+
+        var_dump('obj created');
+    }
 
     public function open()
     {
         $this->isOpen = true;
     }
+    public function __call($name, $arguments)
+    {
+        var_dump($name, $arguments);
+    }
+    public function __toString()
+    {
+        return 'im a box';
+    }
+    public function __get($name)
+    {
+        var_dump($name);
+        return 'cool value';
+    }
+    public function __set($name, $value)
+    {
+        var_dump($name, $value);
+    }
+    public function __destruct()
+    {
+        var_dump('obj destroyed');
+    }
+    public function __invoke()
+    {
+        var_dump('im a function');
+    }
 }
-$num1 = 1;
-$num2 = $num1;
-$num1 = 2;
-var_dump($num1, $num2);
-
-$box1 = new Box();
-$box1->width = 1;
-$box2 = clone $box1;
-$box1->width = 2;
-var_dump($box1->width, $box2->width);
-
-$numbers = [1, 2, 3, 4, 5];
-
-
-foreach ($numbers as &$n) {
-    $n += 1;
+function test()
+{
+    $box2 = new Box(2, 4, 6);
 }
-var_dump($numbers);
+test();
+
+$box1 = new Box(1, 2, 3);
+var_dump($box1);
+echo $box1;
+$box1->mew = 'lol';
+var_dump($box1->coolio);
+$box1();
+$box1->hello(1, 'asdas', 3);
